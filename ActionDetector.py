d025cad5d72554874fcd86698c4334c086303dd9
hdl2 <451143003@qq.com>
2017-10-25 05:55:58 +0800

    redefine using inheriting

--- ActionDetector.py
+++ ActionDetector.py
@@ -43,77 +43,78 @@ class Raw_Dataset(Dataset):
     def __len__(self):
         return len(os.listdir("SmallImage/"))
 
-class MyVGG(nn.Module):
-    '''
-    Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-    ReLU (inplace)
-    MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
-    Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-    ReLU (inplace)
-    MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
-    Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-    ReLU (inplace)
-    Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-    ReLU (inplace)
-    MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
-    Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-    ReLU (inplace)
-    Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-    ReLU (inplace)
-    MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
-    Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-    ReLU (inplace)
-    Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-    ReLU (inplace)
-    MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
-    '''
-    def __init__(self, features, num_classes=1000):
-        super(MyVGG, self).__init__()
-        self.features = features
-        self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes),
-        )
-        self._initialize_weights()
-
-    def forward(self, x):
-        for layer in self.features:
-            print(layer)
-            x = layer(x)
-            
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-                if m.bias is not None:
-                    m.bias.data.zero_()
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                n = m.weight.size(1)
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
+# class MyVGG(nn.Module):
+#     '''
+#     Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     ReLU (inplace)
+#     MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
+#     Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     ReLU (inplace)
+#     MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
+#     Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     ReLU (inplace)
+#     Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     ReLU (inplace)
+#     MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
+#     Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     ReLU (inplace)
+#     Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     ReLU (inplace)
+#     MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
+#     Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     ReLU (inplace)
+#     Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+#     ReLU (inplace)
+#     MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
+#     '''
+#     def __init__(self, features, num_classes=1000):
+#         super(MyVGG, self).__init__()
+#         self.features = features
+#         self.classifier = nn.Sequential(
+#             nn.Linear(512 * 7 * 7, 4096),
+#             nn.ReLU(True),
+#             nn.Dropout(),
+#             nn.Linear(4096, 4096),
+#             nn.ReLU(True),
+#             nn.Dropout(),
+#             nn.Linear(4096, num_classes),
+#         )
+#         self._initialize_weights()
+#
+#     def forward(self, x):
+#         for layer in self.features:
+#             print(layer)
+#             x = layer(x)
+#
+#         x = x.view(x.size(0), -1)
+#         x = self.classifier(x)
+#         return x
+#
+#     def _initialize_weights(self):
+#         for m in self.modules():
+#             if isinstance(m, nn.Conv2d):
+#                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+#                 m.weight.data.normal_(0, math.sqrt(2. / n))
+#                 if m.bias is not None:
+#                     m.bias.data.zero_()
+#             elif isinstance(m, nn.BatchNorm2d):
+#                 m.weight.data.fill_(1)
+#                 m.bias.data.zero_()
+#             elif isinstance(m, nn.Linear):
+#                 n = m.weight.size(1)
+#                 m.weight.data.normal_(0, 0.01)
+#                 m.bias.data.zero_()
 
 class MyVGG(vgg.VGG):
     def __init__(self, features):
-        super(MyVGG, self).__init__(features, num_class=1000)
+        super(MyVGG, self).__init__(features, num_classes=1000)
         self.features = features
 
     def forward(self, x):
         for layer in self.features:
-            print(layer)
             x = layer(x)
+            if isinstance(layer, torch.nn.modules.conv.Conv2d):
+                print(x.data.shape)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
