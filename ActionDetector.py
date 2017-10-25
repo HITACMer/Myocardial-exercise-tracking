b6037d394e7dde45841b446db38847e7bbf3a33f
hdl2 <451143003@qq.com>
2017-10-26 06:21:24 +0800

    Finish special feature extract, now we need to change the ActionDetector to judge which point and time the image moved violently.

--- ActionDetector.py
+++ ActionDetector.py
@@ -4,25 +4,24 @@ import pandas as pd
 from skimage import io, transform
 import numpy as np
 import matplotlib.pyplot as plt
-from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 import MaUtilities as mu
-import torch
+import numpy as np
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
-from torch.autograd import Variable
-from torchvision.models import vgg
-import numpy as np
 import torchvision
-from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
-import torch.utils.model_zoo as model_zoo
-
 import time
 import os
 from PIL import Image
 import math
+from torch.utils.data import Dataset, DataLoader
+import torch
+from torch.autograd import Variable
+from torchvision.models import vgg
+from torchvision import datasets, models, transforms
+import torch.utils.model_zoo as model_zoo
 
 class Raw_Dataset(Dataset):
     def __init__(self, transform=None):
@@ -30,9 +29,10 @@ class Raw_Dataset(Dataset):
         self.transform = transform
 
     def __getitem__(self, item):
-        image = Image.open("SmallImage/%d.png" % (item + 1))
+        image = Image.open("P1_SmallImage/%d.png" % (item + 1))
         image = image.resize((224, 224))
         image = np.asarray(image)
+        #image = mu.copy_2D_to_3D(image, 3)
         image = image.reshape(224, 224, 3)
         #mu.show_detail(image)
         if self.transform is not None:
@@ -41,100 +41,51 @@ class Raw_Dataset(Dataset):
         return image
 
     def __len__(self):
-        return len(os.listdir("SmallImage/"))
-
-# class MyVGG(nn.Module):
-#     '''
-#     Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-#     ReLU (inplace)
-#     MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
-#     Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-#     ReLU (inplace)
-#     MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
-#     Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-#     ReLU (inplace)
-#     Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-#     ReLU (inplace)
-#     MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
-#     Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-#     ReLU (inplace)
-#     Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-#     ReLU (inplace)
-#     MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
-#     Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-#     ReLU (inplace)
-#     Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-#     ReLU (inplace)
-#     MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
-#     '''
-#     def __init__(self, features, num_classes=1000):
-#         super(MyVGG, self).__init__()
-#         self.features = features
-#         self.classifier = nn.Sequential(
-#             nn.Linear(512 * 7 * 7, 4096),
-#             nn.ReLU(True),
-#             nn.Dropout(),
-#             nn.Linear(4096, 4096),
-#             nn.ReLU(True),
-#             nn.Dropout(),
-#             nn.Linear(4096, num_classes),
-#         )
-#         self._initialize_weights()
-#
-#     def forward(self, x):
-#         for layer in self.features:
-#             print(layer)
-#             x = layer(x)
-#
-#         x = x.view(x.size(0), -1)
-#         x = self.classifier(x)
-#         return x
-#
-#     def _initialize_weights(self):
-#         for m in self.modules():
-#             if isinstance(m, nn.Conv2d):
-#                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-#                 m.weight.data.normal_(0, math.sqrt(2. / n))
-#                 if m.bias is not None:
-#                     m.bias.data.zero_()
-#             elif isinstance(m, nn.BatchNorm2d):
-#                 m.weight.data.fill_(1)
-#                 m.bias.data.zero_()
-#             elif isinstance(m, nn.Linear):
-#                 n = m.weight.size(1)
-#                 m.weight.data.normal_(0, 0.01)
-#                 m.bias.data.zero_()
-
-class MyVGG(vgg.VGG):
-    def __init__(self, features):
-        super(MyVGG, self).__init__(features, num_classes=1000)
-        self.features = features
-
-    def forward(self, x):
-        for layer in self.features:
-            x = layer(x)
-            if isinstance(layer, torch.nn.modules.conv.Conv2d):
-                print(x.data.shape)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
+        return len(os.listdir("P1_SmallImage/"))
 
 transformer = transforms.Compose([
     transforms.ToTensor(), # range [0, 255] -> [0.0,1.0]
-    transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+    #transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
     ]
 )
-a = Raw_Dataset()
-dataloader = DataLoader(Raw_Dataset(transformer), batch_size=1)
 
+class MyVGG(vgg.VGG):
+    # Do NOT change variable name "features", because pretrained weights link to "features"
+    def __init__(self, features):
+        super(MyVGG, self).__init__(features, num_classes=1000)
+        self.features = features
+        # collect all feature images
+        self.feature_images = []
 
-pretrained_model = models.vgg11(pretrained=True).cuda()
+    def forward(self, x):
+        current_feature_images = []
+        # Split VGG's sequential up to alone layers so that we can identify conv layers
+        for layer in self.features:
+            x = layer(x)
+            if isinstance(layer, torch.nn.modules.conv.Conv2d):
+                # get features
+                print(x.data.shape)
+                current_feature_images.append(x.data)
+        self.feature_images.append(current_feature_images)
+        #x = x.view(x.size(0), -1)
+        #x = self.classifier(x)
+        return x
+
+    def get_features(self):
+        return self.feature_images
+
 my_model = MyVGG(vgg.make_layers(vgg.cfg['A'])).cuda()
 my_model.load_state_dict(model_zoo.load_url(vgg.model_urls['vgg11']))
 
+dataloader = DataLoader(Raw_Dataset(transformer), batch_size=1)
 
 for i, (data) in enumerate(dataloader):
     data = Variable(data).cuda()
-    #print(data.data)
     output = my_model(data)
-    print(i)
+
+features = my_model.get_features()
+print(len(features))
+#print(features[2][3])
+
+
+torch.save(features, "features")
---
+++ FeatureAnalyzer.py
@@ -0,0 +1,63 @@
+import os
+import torch
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision import transforms, utils
+import MaUtilities as mu
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torchvision
+import matplotlib.pyplot as plt
+import time
+import os
+from PIL import Image
+import math
+from torch.utils.data import Dataset, DataLoader
+import torch
+from torch.autograd import Variable
+from torchvision.models import vgg
+from torchvision import datasets, models, transforms
+import torch.utils.model_zoo as model_zoo
+import cv2
+
+plt.ion()
+features = torch.load("features")
+print(len(features))
+
+frame = 3
+conv = 0
+
+a = features[frame][conv].cpu().numpy()[0, ...]
+mu.show_detail(a)
+special_feature = [cv2.threshold(features[frame][1].cpu().numpy()[0][47], 0.5, 1, cv2.THRESH_BINARY) for frame in range(220)]
+torch.save(special_feature, "SpecialLayerFeature")
+
+for frame in range(220):
+    current_feature = features[frame][1].cpu().numpy()[0][47]
+    (T, current_feature) = cv2.threshold(current_feature, 0.5, 1, cv2.THRESH_BINARY)#阈值化处理，阈值为：155
+    original_image = Image.open("SmallImage/%d.png" % (frame+1))
+    preprocessed_image = Image.open("P1_SmallImage/%d.png" % (frame+1))
+    plt.ioff()
+    plt.subplot(131)
+    plt.imshow(original_image)
+    plt.subplot(132)
+    plt.imshow(preprocessed_image)
+    plt.subplot(133)
+    plt.imshow(current_feature)
+    plt.title("%d" % frame)
+    #plt.ion()
+    plt.show()
+    plt.pause(0.05)
+
+assert 0
+#mu.display(a[0, 0, ...])
+for index in range(a.shape[1]):
+    plt.imshow(a[index, ...])
+    plt.title("%d" % index)
+    plt.pause(0.5)
+mu.display()
+print(a.shape)
